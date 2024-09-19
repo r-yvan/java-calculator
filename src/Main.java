a8e3538390f9529573aefdcb30c5246204d8c284
r-yvan <yvankiliye.rubuto@gmail.com>
2024-09-19 15:22:24 +0200

    Commit

--- src/Main.java
+++ src/Main.java
@@ -2,24 +2,24 @@ import java.util.Scanner;
 import java.math.*;
 
 class Calculator {
-    public int add(int n1, int n2) {
+    public float add(float n1, float n2) {
         return n1 + n2;
     }
 
-    public int multiply(int n1, int n2) {
+    public float multiply(float n1, float n2) {
         return n1 * n2;
     }
 
-    public int subtract(int n1, int n2) {
+    public float subtract(float n1, float n2) {
         return n1 - n2;
     }
 
-    public int divide(int n1, int n2) {
+    public float divide(float n1, float n2) {
         return n1 / n2;
     }
 
-    public int power(int n1, int n2) {
-        return n1 ^ n2;
+    public double power(double n1, double n2) {
+        return Math.pow(n1, n2);
     }
 }
 
@@ -28,28 +28,38 @@ public class Main {
         Scanner scanner = new Scanner(System.in);
         Calculator calc = new Calculator();
         while (true) {
+
             System.out.println("Enter the First Number: ");
-            int num1 = scanner.nextInt();
+            float num1 = scanner.nextFloat();
 
             System.out.println("Enter the Second Number: ");
-            int num2 = scanner.nextInt();
+            float num2 = scanner.nextFloat();
 
-            System.out.println("Enter an Operation to be done on the numbers: ");
+            System.out.println("Use the following signs: ");
+
+            System.out.println();
+
             System.out.println("[+]: For Addition ");
             System.out.println("[-]: For Subtraction ");
             System.out.println("[*]: For Multiplication ");
             System.out.println("[/]: For Division ");
+            System.out.println("[^]: For Powering ");
 
+            System.out.println();
+
+            System.out.println("Enter an Operation to be done on the numbers: ");
             String choice = scanner.next();
 
-            if (! choice.isEmpty()) {
-                switch (choice) {
+            if (!choice.isEmpty()) {
+                float result = switch (choice) {
                     case "+" -> calc.add(num1, num2);
                     case "-" -> calc.subtract(num1, num2);
                     case "/" -> calc.divide(num1, num2);
                     case "*" -> calc.multiply(num1, num2);
-                    default -> System.out.println("You entered and Invalid Operation !!");
-                }
+                    case "^" -> (float) calc.power(num1, num2);
+                    default -> 404;
+                };
+                System.out.println("The result is: [ " + result + " ]");
             }
         }
     }
